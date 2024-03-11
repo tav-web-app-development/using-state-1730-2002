f8b603805c16ed5dae5d3d3f138052be0f2ac061
ishita <ishita.email@example.com>
2024-03-11 19:55:40 -0400

    done

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/Yx5XQOLn)
+# Using State Assignment
+
+## Fix the image carousel in `productCard`
+
+1. Have the product image change when the next or previous button are clicked
+1. Disable the next and previous button when there are no more images left to be shown
+
+## Fix the Show Description Button
+
+1. Fix the show description button to toggle between showing product details or hiding them when clicked
+1. When showing product details, the button should say "Hide Description". When details are hidden, it should say "Show Description"
+
+## Fix Add To Cart Button
+
+1. When clicking on the add to cart button, increment the product counter and fire an alert that will say "You have {productCount} added to you cart "
+
+## Fix Product Card Container
+
+1. Fix the small error in ProductCardContainer.jsx
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+export default function ProductCard({ product }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [itemsInCart, setItemsInCart] = useState(0);
+  const [showDescription, setShowDescription] = useState(false);
+
+  
+
+  const handleNextButtonClick = () => {
+    setCurrentImageIndex(prevIndex =>
+      prevIndex === product.imageUrls.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePreviousButtonClick = () => {
+    setCurrentImageIndex(prevIndex =>
+      prevIndex === 0 ? product.imageUrls.length - 1 : prevIndex - 1
+    );
+  };
+  const toggleDescription = () => {
+    setShowDescription(prevState => !prevState);
+  };
+  const handleAddToCartClick = () => {
+    setProductCount(prevCount => prevCount + 1);
+    alert('You have ${productCount + 1} items added to your cart');
+  };
+
+  return (
+    <>
+      <div id="image-carousel">
+        <img
+          src={product.imageUrls[currentImageIndex] + " " + product.name}
+          alt={product.name}
+        />
+        <button onClick={handleNextButtonClick} disabled={product.imageUrls.length <= 1}>Next</button>
+        <button onClick={handlePreviousButtonClick} disabled={product.imageUrls.length <= 1}>Previous</button>
+      </div>
+
+      <h3>{product.name}</h3>
+      {showDescription && <p>{product.description}</p>}
+      <button onClick={toggleDescription}>
+        {showDescription ? "Hide Description" : "Show Description"}</button>
+      <div className="price">${product.price}</div>
+
+      <button onClick={handleAddToCartClick}>Add to Cart</button>
+
+      {!product.isInStock && "The product is out of stock"}
+    </>
+  );
+}
